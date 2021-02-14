d0a9ca7c575e3ad1c9f597c14ded77e38efb18d1
Dominic Skeete <72157493+daskeete@users.noreply.github.com>
2021-02-14 17:12:48 -0500

    Update Formal_Response_Week_2.py

--- Formal_Response_Week_2.py
+++ Formal_Response_Week_2.py
@@ -1,40 +1,74 @@
+import numpy as np
+np.set_printoptions(suppress=True, precision=15)
+
+#%%
+
 import numpy as np, tensorflow as tf, matplotlib.pyplot as plt
 from tensorflow import keras
 mnist = tf.keras.datasets.mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
-np.set_printoptions(suppress=True, precision=20)
 
-training_images.shape
-training_labels.shape
-test_images.shape
+#%%
 
-training_images = training_images / np.max(np.unique(training_images))
-test_images = test_images / np.max(np.unique(test_images))
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+
+
+#%%
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),
                                     tf.keras.layers.Dense(128, activation=tf.nn.relu),
-                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])                                  
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+#%%
+
 model.compile(optimizer = tf.keras.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(training_images, training_labels, epochs=10)                
-model.evaluate(test_images, test_labels)
-classifications = model.predict(test_images)
-classifications[0]
 
-np.argmax(classifications[0]) #returns indices of maximum values
+model.fit(training_images, training_labels, epochs=10)
 
-plt.imshow(test_images[0])
-plt.savefig('number_7')
+#%%
 
-b = list(classifications[0])
-for i in range(len(classifications[0])):
-    if b[i] < max(classifications[0]):
-        b[i]=0.1
-bars = np.unique(test_labels)
-color = ['grey' if (x < max(classifications[0])) else 'blue' for x in classifications[0]]
-plt.bar(bars,b,color=color)
-plt.xticks(bars)
-plt.tick_params(labelleft=False)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
 
-plt.savefig('probability_graph')
+#%%
+
+class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+i=0
+
+#%%
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
+predictions[i]
+
+#%%
+
+plt.title('My Image Plot')
+plt.imshow(test_images[i])
+plt.savefig('number_')
 plt.show()
+
+
+#%%
+
+#bars = np.unique(test_labels)
+color = ['grey' if (x < max(predictions[i])) else 'blue' for x in predictions[i]]
+plt.bar(class_names,predictions[i],color=color)
+plt.xticks(class_names)
+#plt.tick_params(labelleft=False)
+plt.title('My Probability Graph')
+plt.savefig('probability_graph')
+
+plt.show()
+
+#%%
+
+predictions[i]
+
+
+#%%
+
+np.argmax(predictions[i])
